128df79e1f5da6a19cd52f31f6436cffb40623f3
valentinnn1 <valentinnn1@outlook.com>
2019-03-31 20:29:05 +0300

    Added Programming Basics, First steps in coding

---
+++ Programming Basics/01. First Steps In Coding/03. Nums 1 to 20 easy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Nums_1_to_20_easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+
+            {
+
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
---
+++ Programming Basics/01. First Steps In Coding/04. Rectangle Area/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A:");
+            var a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter B:");
+            var b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("The area is");
+            Console.WriteLine(a * b);
+        }
+    }
+}
