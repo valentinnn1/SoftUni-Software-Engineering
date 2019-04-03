aa36d6999e2bb2ac6f48905dbe2bdcbb6b4ab987
valentinnn1 <valentinnn1@outlook.com>
2019-04-03 19:18:08 +0300

    Added 06

---
+++ Programming Basics/06. Nested Conditional Statements/07. Trade Comissions/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace _07.Trade_Comissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine().ToLower();
+            double sales = double.Parse(Console.ReadLine());
+
+            double commission = -1.0;
+
+            if (town == "sofia")
+            {
+                if (0 <= sales && sales <= 500)
+                {
+                    commission = 0.05;
+                }
+                else if (500 < sales && sales <= 1000)
+                {
+                    commission = 0.07;
+                }
+                else if (1000 < sales && sales <= 10000)
+                {
+                    commission = 0.08;
+                }
+                else if (sales > 10000)
+                {
+                    commission = 0.12;
+                }
+            }
+            else if (town == "varna")
+            {
+                if (0 <= sales && sales <= 500)
+                {
+                    commission = 0.045;
+                }
+                else if (500 < sales && sales <= 1000)
+                {
+                    commission = 0.075;
+                }
+                else if (1000 < sales && sales <= 10000)
+                {
+                    commission = 0.1;
+                }
+                else if (sales > 10000)
+                {
+                    commission = 0.13;
+                }
+            }
+            else if (town == "plovdiv")
+            {
+                if (0 <= sales && sales <= 500)
+                {
+                    commission = 0.055;
+                }
+                else if (500 < sales && sales <= 1000)
+                {
+                    commission = 0.08;
+                }
+                else if (1000 < sales && sales <= 10000)
+                {
+                    commission = 0.12;
+                }
+                else if (sales > 10000)
+                {
+                    commission = 0.145;
+                }
+            }
+
+            if (sales > 0 && commission > 0)
+            {
+                Console.WriteLine($"{commission * sales}:F2");
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
---
+++ Programming Basics/06. Nested Conditional Statements/10. Match Tickets/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Match_Tickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string ticketCategory = Console.ReadLine();
+            int people = int.Parse(Console.ReadLine());
+            double ticketPrice = 0.00;
+            double moneyForTravel = 0.00;
+            double finalSum = 0;
+
+            if (ticketCategory == "VIP")
+            {
+                ticketPrice = 499.99;
+            }
+            else
+            {
+                ticketPrice = 249.99;
+            }
+
+            if (people >= 1 && people < 5)
+            {
+                moneyForTravel = budget * 0.75;
+                finalSum = budget - moneyForTravel;
+            }
+            else if (people >= 5 && people < 10)
+            {
+                moneyForTravel = budget * 0.60;
+                finalSum = budget - moneyForTravel;
+            }
+            else if (people >= 10 && people < 25)
+            {
+                moneyForTravel = budget * 0.50;
+                finalSum = budget - moneyForTravel;
+            }
+            else if (people >= 25 && people < 50)
+            {
+                moneyForTravel = budget * 0.40;
+                finalSum = budget - moneyForTravel;
+            }
+            else if (people >= 50)
+            {
+                moneyForTravel = budget * 0.25;
+                finalSum = budget - moneyForTravel;
+            }
+
+            finalSum = finalSum - (people * ticketPrice);
+
+            if (finalSum >= 0)
+            {
+                Console.WriteLine($"Yes! You have {finalSum:F2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {Math.Abs(finalSum):F2} leva.");
+            }
+        }
+    }
+}
