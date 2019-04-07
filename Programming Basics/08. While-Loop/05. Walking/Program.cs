eaa79ad374736117b94a41e5b990f7ea60053f8e
valentinnn1 <valentinnn1@outlook.com>
2019-04-07 14:08:12 +0300

    Added 08 Part 2

---
+++ Programming Basics/08. While-Loop/05. Walking/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int purpose = 10_000;
+            int stepsSum = 0;
+            string output = string.Empty;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Going home")
+                {
+                    int finalSteps = int.Parse(Console.ReadLine());
+                    stepsSum += finalSteps;
+                    break;
+                }
+                else
+                {
+                    int currentSteps = int.Parse(command);
+                    stepsSum += currentSteps;
+
+                    if (stepsSum >= purpose)
+                        break;
+                }
+            }
+            if (stepsSum >= purpose)
+                output = "Goal reached! Good job!";
+            else
+                output = $"{purpose - stepsSum} more steps to reach goal.";
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Programming Basics/08. While-Loop/06. Account Balance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06.Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broiVnoski = int.Parse(Console.ReadLine());
+            decimal balance = 0.0m;
+
+            int counter = 1;
+            decimal vnoska;
+
+            while (counter <= broiVnoski)
+            {
+                vnoska = decimal.Parse(Console.ReadLine());
+                if (vnoska < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {vnoska:F2}");
+                counter++;
+                balance += vnoska;
+            }
+            Console.WriteLine($"Total: {balance:F2}");
+        }
+    }
+}
