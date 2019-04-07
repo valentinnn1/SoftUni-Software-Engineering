6a0ec0c6671b5e0cce4e652a7020fc0d227332e3
valentinnn1 <valentinnn1@outlook.com>
2019-04-07 14:08:35 +0300

    Added 08 Part 3

---
+++ Programming Basics/08. While-Loop/08. Sequence 2k+1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Sequence_2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            while (true)
+            {
+                Console.WriteLine(num);
+                num = 2 * num + 1;
+                if (num > n)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
