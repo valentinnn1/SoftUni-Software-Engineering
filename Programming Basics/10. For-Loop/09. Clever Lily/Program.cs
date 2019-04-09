59a298c9c96970f5bf54ceec2dbb305cbc3eb842
valentinnn1 <valentinnn1@outlook.com>
2019-04-09 14:29:38 +0300

    Added 10 Part 3

---
+++ Programming Basics/10. For-Loop/09. Clever Lily/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Clever_Lily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washingMachinePrice = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+            int toysCounter = 0;
+            int moneyFromGifts = 0;
+            int badBrotherCounter = 0;
+
+            for (int i = 1; i <= age; i++)
+            {
+                if (i%2==0)
+                {
+                    badBrotherCounter++;
+                    moneyFromGifts += 10 * badBrotherCounter;
+                }
+                else
+                {
+                    toysCounter++;
+                }
+            }
+            int moneyForToys = toysCounter * toyPrice;
+            int totalMoney = (moneyForToys + moneyFromGifts) - badBrotherCounter;
+
+            if (totalMoney >= washingMachinePrice)
+            {
+                Console.WriteLine($"Yes! {(totalMoney - washingMachinePrice):F2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {(washingMachinePrice-totalMoney):F2}");
+            }
+        }
+    }
+}
