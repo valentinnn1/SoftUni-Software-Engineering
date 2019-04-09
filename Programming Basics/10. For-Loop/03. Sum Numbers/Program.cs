9daa3cbf051ba2e8bd4e46648749a75805dcd1eb
valentinnn1 <valentinnn1@outlook.com>
2019-04-09 07:30:05 +0300

    Added 10 Part 1

---
+++ Programming Basics/10. For-Loop/03. Sum Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            double sum = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                double singleNumber = double.Parse(Console.ReadLine());
+                sum += singleNumber;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
