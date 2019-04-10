43ea988b37e1b5f8d49ead7b25fccbf8a3f1dae7
valentinnn1 <valentinnn1@outlook.com>
2019-04-10 15:57:06 +0300

    Added 11 Part 2

---
+++ Programming Basics/11. Exercise-For Loop/05. Histogram/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int numbersTo200 = 0;
+            int numbersFrom200To399 = 0;
+            int numbersFrom400To599 = 0;
+            int numbersFrom600To799 = 0;
+            int numbers800 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 200)
+                {
+                    numbersTo200++;
+                }
+                else if (number <= 399)
+                {
+                    numbersFrom200To399++;
+                }
+                else if (number <= 599)
+                {
+                    numbersFrom400To599++;
+                }
+                else if (number < 800)
+                {
+                    numbersFrom600To799++;
+                }
+                else
+                {
+                    numbers800++;
+                }
+            }
+            Console.WriteLine($"{(numbersTo200 * 100.0 / n):F2}%");
+            Console.WriteLine($"{(numbersFrom200To399 * 100.0 / n):F2}%");
+            Console.WriteLine($"{(numbersFrom400To599 * 100.0 / n):F2}%");
+            Console.WriteLine($"{(numbersFrom600To799 * 100.0 / n):F2}%");
+            Console.WriteLine($"{(numbers800 * 100.0 / n):F2}%");
+        }
+    }
+}
---
+++ Programming Basics/11. Exercise-For Loop/06. Divide without remainder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06.Divide_without_remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int divideBy2 = 0;
+            int divideBy3 = 0;
+            int divideBy4 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0)
+                {
+                    divideBy2++;
+                }
+                if (number % 3 == 0)
+                {
+                    divideBy3++;
+                }
+                if (number % 4 == 0)
+                {
+                    divideBy4++;
+                }
+            }
+
+            Console.WriteLine($"{(divideBy2 * 100.0 / n):F2}%");
+            Console.WriteLine($"{(divideBy3 * 100.0 / n):F2}%");
+            Console.WriteLine($"{(divideBy4 * 100.0 / n):F2}%");
+        }
+    }
+}
