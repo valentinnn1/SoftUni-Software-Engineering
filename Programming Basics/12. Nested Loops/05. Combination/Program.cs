b51e3b730f4bb44e46e48212225362ed4d8084a9
valentinnn1 <valentinnn1@outlook.com>
2019-04-11 18:30:23 +0300

    Added 12 Part 2

---
+++ Programming Basics/12. Nested Loops/05. Combination/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Combination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            
+            for (int x1 = 0; x1 <= 25; x1++)
+            {
+                for (int x2 = 0; x2 <= 25; x2++)
+                {
+                    for (int x3 = 0; x3 <= 25; x3++)
+                    {
+                        for (int x4 = 0; x4 <= 25; x4++)
+                        {
+                            for (int x5 = 0; x5 <= 25; x5++)
+                            {
+                                if (x1 + x2 + x3 + x4 + x5 == n)
+                                {
+                                    counter++;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ Programming Basics/12. Nested Loops/06. Travelling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = string.Empty;
+            while ((line = Console.ReadLine()) != "End")
+            {
+                string destination = line;
+                double neededMoney = double.Parse(Console.ReadLine());
+
+                double money = 0;
+                string savings = string.Empty;
+                while (true)
+                {
+                    savings = Console.ReadLine();
+                    if (savings == "End")
+                    {
+                        return;
+                    }
+
+                    money += double.Parse(savings);
+
+                    if (money >= neededMoney)
+                    {
+                        Console.WriteLine($"Going to {destination}!");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
