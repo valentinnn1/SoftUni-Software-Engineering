f0393a1842e8524be0be1d0d221ece857f023568
valentinnn1 <valentinnn1@outlook.com>
2019-04-11 18:30:53 +0300

    Added 12 Part 3

---
+++ Programming Basics/12. Nested Loops/07. Name Wars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Name_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int maxCombination = int.MinValue;
+            string winner = string.Empty;
+
+            while (input != "STOP")
+            {
+                int currentSum = 0;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    currentSum += input[i];
+                }
+                if (currentSum > maxCombination)
+                {
+                    maxCombination = currentSum;
+                    winner = $"Winner is {input} - {currentSum}!";
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(winner);
+        }
+    }
+}
---
+++ Programming Basics/12. Nested Loops/08. Cookie factory/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Cookie_factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counts = int.Parse(Console.ReadLine());
+            string command = string.Empty;
+            bool hasFlour = false;
+            bool hasEggs = false;
+            bool hasSugar = false;
+            int i = 1;
+
+            while (counts > 0)
+            {
+                command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "flour":
+                        hasFlour = true;
+                        break;
+                    case "eggs":
+                        hasEggs = true;
+                        break;
+                    case "sugar":
+                        hasSugar = true;
+                        break;
+                }
+
+                if (command == "Bake!")
+                {
+
+                    if (hasFlour && hasEggs && hasSugar)
+                    {
+
+                        Console.WriteLine("Baking batch number {0}...", i);
+                        counts--;
+                        i++;
+                        hasFlour = hasEggs = hasSugar = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The batter should contain flour, eggs and sugar!");
+                    }
+                }
+            }
+
+        }
+
+    }
+}
