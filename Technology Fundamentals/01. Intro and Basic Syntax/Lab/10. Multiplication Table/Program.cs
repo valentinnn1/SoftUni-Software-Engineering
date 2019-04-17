57c9dbbd4c085cef163d993e249ca2c0e2237700
valentinnn1 <valentinnn1@outlook.com>
2019-04-17 17:30:27 +0300

    Added 01, Part 4

---
+++ Technology Fundamentals/01. Intro and Basic Syntax/Lab/10. Multiplication Table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _10._Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int product = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                product = number * i;
+                Console.WriteLine($"{number} X {i} = {product}");
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals/01. Intro and Basic Syntax/Lab/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _11._Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int product = 0;
+
+            if (secondNumber >= 1 && secondNumber <= 10)
+            {
+                for (int i = secondNumber; i <= 10; i++)
+                {
+                    product = firstNumber * i;
+                    Console.WriteLine($"{firstNumber} X {i} = {product}");
+                }
+            }
+            else
+            {
+                product = firstNumber * secondNumber;
+                Console.WriteLine($"{firstNumber} X {secondNumber} = {product}");
+            }
+        }
+    }
+}
