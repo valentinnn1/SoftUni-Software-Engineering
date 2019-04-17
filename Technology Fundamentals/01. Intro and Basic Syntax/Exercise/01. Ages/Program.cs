877d1ca7b0fce3431d17ff440907907613b0707f
valentinnn1 <valentinnn1@outlook.com>
2019-04-17 17:34:33 +0300

    Added Part 1

---
+++ Technology Fundamentals/01. Intro and Basic Syntax/Exercise/01. Ages/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            if (age == 0 || age > 0 && age < 3)
+            {
+                Console.WriteLine("baby");
+            }
+            if (age > 2 && age <= 13)
+            {
+                Console.WriteLine("child");
+            }
+            if (age >= 14 && age <= 19)
+            {
+                Console.WriteLine("teenager");
+            }
+            if (age >= 20 && age <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+            if (age >= 66)
+            {
+                Console.WriteLine("elder");
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals/01. Intro and Basic Syntax/Exercise/03. Vacation/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _03._Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            string typeOfGroup = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+
+            double price = 0.00;
+
+            if (dayOfWeek == "Friday")
+            {
+                if (typeOfGroup == "Students")
+                {
+                    price += 8.45;
+                }
+                else if (typeOfGroup == "Business")
+                {
+                    price += 10.90;
+                }
+                else if (typeOfGroup == "Regular")
+                {
+                    price += 15.00;
+                }
+            }
+            if (dayOfWeek == "Saturday")
+            {
+                if (typeOfGroup == "Students")
+                {
+                    price += 9.80;
+                }
+                else if (typeOfGroup == "Business")
+                {
+                    price += 15.60;
+                }
+                else if (typeOfGroup == "Regular")
+                {
+                    price += 20.00;
+                }
+            }
+            if (dayOfWeek == "Sunday")
+            {
+                if (typeOfGroup == "Students")
+                {
+                    price += 10.46;
+                }
+                else if (typeOfGroup == "Business")
+                {
+                    price += 16.00;
+                }
+                else if (typeOfGroup == "Regular")
+                {
+                    price += 22.50;
+                }
+            }
+            double totalPrice = 0.00;
+            totalPrice = price * people;
+
+            if (people >= 30 && typeOfGroup == "Students")
+            {
+                totalPrice = people * (price - (price * 0.15));
+            }
+            else if (people >= 100 && typeOfGroup == "Business")
+            {
+                totalPrice = price * (people - 10);
+            }
+            else if (people >= 10 && people <= 20 && typeOfGroup == "Regular")
+            {
+                totalPrice = people * (price - (price * 0.5));
+            }
+
+            Console.WriteLine($"Total price: {totalPrice:F2}");
+        }
+    }
+}
