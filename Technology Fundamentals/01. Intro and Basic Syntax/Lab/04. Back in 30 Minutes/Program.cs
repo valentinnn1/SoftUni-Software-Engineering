4e5c967d332879a85f218d7b336d4dabfefd0697
valentinnn1 <valentinnn1@outlook.com>
2019-04-17 17:29:48 +0300

    Added 01, Part 2

---
+++ Technology Fundamentals/01. Intro and Basic Syntax/Lab/04. Back in 30 Minutes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04._Back_in_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());      
+            int minutes = int.Parse(Console.ReadLine());
+
+            var time = new TimeSpan(hours, minutes + 30, 0);
+
+            Console.WriteLine($"{time:h\\:mm}");
+        }
+    }
+}
