96395704f5eb0408d049f7ce1254acca3a12aba8
valentinnn1 <valentinnn1@outlook.com>
2019-04-18 21:05:32 +0300

    Added Part 2

---
+++ Technology Fundamentals/01. Intro and Basic Syntax/Exercise/05. Login/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05._Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = "";
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            string currentPassword = Console.ReadLine();
+            int counter = 0;
+            while (currentPassword != password)
+            {
+                counter += 1;
+                if (counter == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!"); return;
+                }
+                currentPassword = Console.ReadLine();
+
+                Console.WriteLine("Incorrect password. Try again.");
+            }
+
+            Console.WriteLine($"User {username} logged in.");
+        }
+    }
+}
---
+++ Technology Fundamentals/01. Intro and Basic Syntax/Exercise/06. Strong number/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _06._Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            int factoriel = 1;
+
+            for (int i = number; i >= 1; i /= 10)
+            {
+                int digit = i % 10;
+                if (digit == 0)
+                {
+                    factoriel = 1;
+                    sum += factoriel;
+                    continue;
+                }
+                for (int j = 1; j <= digit; j++)
+                {
+                    factoriel *= j;
+                }
+                sum += factoriel;
+                factoriel = 1;
+            }
+            if (sum == number)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
