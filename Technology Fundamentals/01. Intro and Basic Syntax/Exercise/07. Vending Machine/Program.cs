7013fc0af479af2e5407633ab4bfff3d4071c87a
valentinnn1 <valentinnn1@outlook.com>
2019-04-18 21:05:55 +0300

    Added Part 3

---
+++ Technology Fundamentals/01. Intro and Basic Syntax/Exercise/07. Vending Machine/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _07._Vending_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double balance = 0;
+            while (input != "Start")
+            {
+                double coin = double.Parse(input);
+
+                if (coin == 0.1 ||
+                    coin == 0.2 ||
+                    coin == 0.5 ||
+                    coin == 1 ||
+                    coin == 2)
+                {
+                    balance += coin;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot accept {coin}");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+            while (input != "End")
+            {
+                double productPrice = 0.00;
+
+                switch (input)
+                {
+                    case "Nuts": productPrice = 2; break;
+                    case "Water": productPrice = 0.7; break;
+                    case "Crisps": productPrice = 1.5; break;
+                    case "Soda": productPrice = 0.8; break;
+                    case "Coke": productPrice = 1; break;
+
+                    default:
+                        Console.WriteLine("Invalid product");
+                        break;
+                }
+                if (balance >= productPrice && productPrice > 0)
+                {
+                    string productTorLower = input.ToLower();
+                    Console.WriteLine($"Purchased {productTorLower}");
+                    balance -= productPrice;
+                }
+                else if (productPrice > 0)
+                {
+                    Console.WriteLine();
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Change: {balance:F2}");
+        }
+    }
+}
---
+++ Technology Fundamentals/01. Intro and Basic Syntax/Exercise/09. Padawan Equipment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09._Padawan_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double priceOfLightsabers = double.Parse(Console.ReadLine());
+            double priceOfRobes = double.Parse(Console.ReadLine());
+            double priceOfBelts = double.Parse(Console.ReadLine());
+            double freeBelts = 0.00;
+
+            for (int i = 1; i <= students; i++)
+            {
+                if (i % 6 == 0)
+                {
+                    freeBelts++;
+                }
+            }
+
+            double totalMoney = priceOfLightsabers * (Math.Ceiling(students + (students * 0.1))) + priceOfRobes * students + priceOfBelts * (students - freeBelts);
+            if (totalMoney <= money)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalMoney:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {(totalMoney - money):F2}lv more.");
+            }
+        }
+    }
+}
