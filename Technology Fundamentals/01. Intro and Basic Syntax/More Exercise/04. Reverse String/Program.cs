7bccf9f1d858a6255b79bf70676f8409670329d9
valentinnn1 <valentinnn1@outlook.com>
2019-04-18 21:11:34 +0300

    Added Part 2

---
+++ Technology Fundamentals/01. Intro and Basic Syntax/More Exercise/04. Reverse String/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04._Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text = Console.ReadLine().ToCharArray();
+            Array.Reverse(text);
+            text.ToString();
+            Console.WriteLine(text);
+        }
+    }
+}
