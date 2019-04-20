0c054e72ed8832f5bb38b2162c80083ccf555e49
valentinnn1 <valentinnn1@outlook.com>
2019-04-20 18:22:11 +0300

    Added Part 3

---
+++ Technology Fundamentals/03. Data Types and Variables/Lab/07. Concat Names/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _07._Concat_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string secondName = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            string newString = firstName + "" + delimiter + "" + secondName;
+
+            Console.WriteLine(newString);
+        }
+    }
+}
