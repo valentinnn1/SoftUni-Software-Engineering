b40676b0b978bccf3063c5bf477b819543fa1f5b
valentinnn1 <valentinnn1@outlook.com>
2019-04-21 10:30:45 +0300

    Added Part 1

---
+++ Technology Fundamentals/03. Data Types and Variables/Exercise/01. Integer Operations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourthNum = int.Parse(Console.ReadLine());
+
+            int sum = firstNum + secondNum;
+            int afterDivide = sum / thirdNum;
+            int result = afterDivide * fourthNum;
+
+            Console.WriteLine(result);
+        }
+    }
+}
