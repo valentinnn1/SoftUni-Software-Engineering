f81fbeec79e0ae34dd039898446f39e7d81d6abb
valentinnn1 <valentinnn1@outlook.com>
2019-04-22 17:51:56 +0300

    Added Part 2

---
+++ Technology Fundamentals/03. Data Types and Variables/More Exercise/05. Decrypting Message/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05._Decrypting_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int counterOfChars = int.Parse(Console.ReadLine());
+            string decryptedMessage = string.Empty;
+
+            for (int i = 0; i < counterOfChars; i++)
+            {
+                char character = char.Parse(Console.ReadLine());
+                int newNumberOfChar = number + (int)(character);
+                char newChar = (char)newNumberOfChar;
+                decryptedMessage += newChar;
+            }
+
+            Console.WriteLine(decryptedMessage);
+        }
+    }
+}
---
+++ Technology Fundamentals/03. Data Types and Variables/More Exercise/06. Balanced Brackets/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _06._Balanced_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int openedBracket = 0;
+            int closedBracket = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "(")
+                {
+                    openedBracket++;
+                    if (openedBracket - closedBracket > 1)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+
+                }
+                else if (input == ")")
+                {
+                    closedBracket++;
+                    if (openedBracket - closedBracket != 0)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                }
+            }
+
+            if (openedBracket == closedBracket)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
