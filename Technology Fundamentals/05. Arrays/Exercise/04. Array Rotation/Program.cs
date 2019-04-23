002e1a2fa6a08446c74fedebcbf306780343106a
valentinnn1 <valentinnn1@outlook.com>
2019-04-23 20:52:01 +0300

    Added Part 2

---
+++ Technology Fundamentals/05. Arrays/Exercise/04. Array Rotation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int rotations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rotations; i++)
+            {
+                int firstNum = myArray[0];
+                for (int j = 0; j < myArray.Length - 1; j++)
+                {
+                    myArray[j] = myArray[j + 1];
+                }
+                myArray[myArray.Length - 1] = firstNum;
+            }
+
+            Console.WriteLine(String.Join(" ", myArray));
+        }
+    }
+}
---
+++ Technology Fundamentals/05. Arrays/Exercise/06. Equal Sum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfIntegers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            for (int i = 0; i < arrayOfIntegers.Length; i++)
+            {
+                int sumLeft = 0;
+                int sumRight = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    sumLeft += arrayOfIntegers[j];
+                }
+                for (int k = i + 1; k < arrayOfIntegers.Length; k++)
+                {
+                    sumRight += arrayOfIntegers[k];
+                }
+                if (sumLeft == sumRight)
+                {
+                    Console.WriteLine($"{i}");
+                    if (arrayOfIntegers[i] == 0 && arrayOfIntegers[i + 1] == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
