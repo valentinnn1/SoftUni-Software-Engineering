62cea65f165b84b8d2ca992ea5e1cf6defebd934
valentinnn1 <valentinnn1@outlook.com>
2019-04-23 20:51:45 +0300

    Added Part 1

---
+++ Technology Fundamentals/05. Arrays/Exercise/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] leftElements = new int[n];
+            int[] rightElements = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] currentArr = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (i % 2 == 0)
+                {
+                    leftElements[i] = currentArr[0];
+                    rightElements[i] = currentArr[1];
+                }
+                else
+                {
+                    leftElements[i] = currentArr[1];
+                    rightElements[i] = currentArr[0];
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", leftElements));
+            Console.WriteLine(String.Join(" ", rightElements));
+        }
+    }
+}
