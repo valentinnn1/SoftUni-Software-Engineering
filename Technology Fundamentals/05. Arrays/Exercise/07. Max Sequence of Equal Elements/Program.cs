83e3340252ee8c60f4a6e65401d918ed915bc064
valentinnn1 <valentinnn1@outlook.com>
2019-04-23 20:52:21 +0300

    Added Part 3

---
+++ Technology Fundamentals/05. Arrays/Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+            int count = 0;
+            int position = 0;
+            int maxCount = 0;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    count++;
+                    if (maxCount < count)
+                    {
+                        position = i - count;
+                        maxCount = count;
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+
+            for (int i = position + 1; i <= position + maxCount + 1; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Technology Fundamentals/05. Arrays/Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program, which prints all unique pairs in an array of integers whose sum is equal to a given number.
+            int[] arr = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+            //result=0;
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] + arr[j] == num)
+                    {
+                        Console.WriteLine($"{arr[i]} {arr[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
