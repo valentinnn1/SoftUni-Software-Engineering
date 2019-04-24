672da83a2c5739f73c9d45986cd73bfcfea3cac6
valentinnn1 <valentinnn1@outlook.com>
2019-04-24 17:45:01 +0300

    Added Part 1

---
+++ Technology Fundamentals/06. Methods/Lab/03. Calculations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            if (command == "add")
+            {
+                AddNums(firstNum, secondNum);
+            }
+            else if (command == "multiply")
+            {
+                MultiplyNums(firstNum, secondNum);
+            }
+            else if (command == "subtract")
+            {
+                SubtractNums(firstNum, secondNum);
+            }
+            else if (command == "divide")
+            {
+                DivideNums(firstNum, secondNum);
+            }
+        }
+
+        private static void DivideNums(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum / secondNum);
+        }
+
+        private static void SubtractNums(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum - secondNum);
+        }
+
+        private static void MultiplyNums(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum * secondNum);
+        }
+
+        private static void AddNums(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum + secondNum);
+        }
+    }
+}
