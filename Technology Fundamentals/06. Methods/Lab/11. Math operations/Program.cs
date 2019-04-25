5e5247a0d5bcb8925e66989cc10b75629bba4266
valentinnn1 <valentinnn1@outlook.com>
2019-04-25 18:26:40 +0300

    Added Part 4

---
+++ Technology Fundamentals/06. Methods/Lab/11. Math operations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            string oper = Console.ReadLine();
+            int secondNum = int.Parse(Console.ReadLine());
+            double result = Calculate(firstNum, oper, secondNum);
+            Console.WriteLine(result);
+        }
+
+        static double Calculate(int firstNum, string oper, int secondNum)
+        {
+            double result = 0.0;
+
+            if (oper == "+")
+            {
+                result = firstNum + secondNum;
+            }
+            else if (oper == "-")
+            {
+                result = firstNum - secondNum;
+            }
+            else if (oper == "*")
+            {
+                result = firstNum * secondNum;
+            }
+            else if (oper == "/")
+            {
+                result = firstNum / secondNum;
+            }
+            return result;
+        }
+    }
+}
