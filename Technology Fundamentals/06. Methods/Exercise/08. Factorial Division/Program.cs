508dc66bce048d7d62734879b0e7e403ab571419
valentinnn1 <valentinnn1@outlook.com>
2019-04-26 10:30:57 +0300

    Added Part 3

---
+++ Technology Fundamentals/06. Methods/Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long firstNumber = long.Parse(Console.ReadLine());
+            long secondNumber = long.Parse(Console.ReadLine());
+            long firstFactorial = GetFactorial(firstNumber);
+            long secondFactorial = GetFactorial(secondNumber);
+
+            double result = (double)firstFactorial / secondFactorial;
+            Console.WriteLine($"{result:F2}");
+        }
+
+        private static long GetFactorial(long firstNumber)
+        {
+            long factorial = 1;
+
+            for (int i = 1; i <= firstNumber; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
---
+++ Technology Fundamentals/06. Methods/Exercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PalindromeInteger();
+        }
+
+        static void PalindromeInteger()
+        {
+            string input = Console.ReadLine();
+
+            if (input == "End")
+            {
+                return;
+            }
+
+            while (input != "END")
+            {
+                char[] reverse = input.ToArray();
+                Array.Reverse(reverse);
+
+                for (int i = 0; i < reverse.Length; i++)
+                {
+                    if (reverse[i] == input[i])
+                    {
+                        Console.WriteLine("true"); break;
+                    }
+                    else if (reverse[i] != input[i])
+                    {
+                        Console.WriteLine("false"); break;
+                    }
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
