30cd9cc4f3c3ea52409bad8f43c8b0e6ada3d275
valentinnn1 <valentinnn1@outlook.com>
2019-04-26 10:30:42 +0300

    Added Part 2

---
+++ Technology Fundamentals/06. Methods/Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int sum = SumMethod(firstNum, secondNum);
+            Console.WriteLine(SubtractMethod(sum, thirdNum));
+        }
+
+        private static int SubtractMethod(int sum, int thirdNum)
+        {
+            return sum - thirdNum;
+        }
+
+        private static int SumMethod(int firstNum, int secondNum)
+        {
+            int sum = firstNum + secondNum;
+            return sum;
+        }
+    }
+}
---
+++ Technology Fundamentals/06. Methods/Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PrintingMiddleChars(input);
+        }
+
+        private static void PrintingMiddleChars(string input)
+        {
+            if (input.Length % 2 == 0)
+            {
+                //1234
+                Console.WriteLine($"{input[input.Length / 2 - 1]}{input[input.Length / 2]}");
+            }
+            else
+            {
+                //123
+                Console.WriteLine($"{input[input.Length / 2]}");
+            }
+        }
+    }
+}
