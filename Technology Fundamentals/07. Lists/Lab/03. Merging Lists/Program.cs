388509cfbf3c9be83867f75214c0fed7225abf46
valentinnn1 <valentinnn1@outlook.com>
2019-04-27 10:25:59 +0300

    Added Part 1

---
+++ Technology Fundamentals/07. Lists/Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var secondList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var result = new List<int>();
+            var maxIndex = Math.Max(firstList.Count, secondList.Count);
+
+            for (int i = 0; i < maxIndex; i++)
+            {
+                if (i < firstList.Count)
+                {
+                    result.Add(firstList[i]);
+                }
+                if (i < secondList.Count)
+                {
+                    result.Add(secondList[i]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
