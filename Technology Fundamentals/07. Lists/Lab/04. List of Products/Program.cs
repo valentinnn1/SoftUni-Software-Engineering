0c16c7cf2e341350fc5a55f9afc4391a40b3fc35
valentinnn1 <valentinnn1@outlook.com>
2019-04-27 10:26:22 +0300

    Added Part 2

---
+++ Technology Fundamentals/07. Lists/Lab/04. List of Products/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<string> items = new List<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                items.Add(Console.ReadLine());
+            }
+
+            items.Sort();
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{items[i]}");
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals/07. Lists/Lab/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Remove_Negatives_and_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var positiveNumbers = Console.ReadLine().Split(' ').Select(int.Parse)
+                .Where(number => number >= 0).ToList();
+
+            positiveNumbers.Reverse();
+
+            if (positiveNumbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", positiveNumbers));
+            }
+        }
+    }
+}
