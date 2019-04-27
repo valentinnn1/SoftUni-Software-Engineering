a297c408ce89a1fe2b4547eb8959305aedd0341b
valentinnn1 <valentinnn1@outlook.com>
2019-04-27 10:30:47 +0300

    Added Part 1

---
+++ Technology Fundamentals/07. Lists/Exercise/01. Train/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int capacityOfWagons = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                List<string> directions = command.Split().ToList();
+
+                if (directions[0] == "Add")
+                {
+                    int passengers = int.Parse(directions[1]);
+                    wagons.Add(passengers);
+                }
+                else
+                {
+                    int passengersToFit = int.Parse(directions[0]);
+
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (wagons[i] + passengersToFit <= capacityOfWagons)
+                        {
+                            wagons[i] += passengersToFit; break;
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
---
+++ Technology Fundamentals/07. Lists/Exercise/03. House Party/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._House_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            List<string> namesOfGuests = new List<string>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+
+                if (tokens.Length == 3)
+                {
+                    if (!namesOfGuests.Contains(tokens[0]))
+                    {
+                        namesOfGuests.Add(tokens[0]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{tokens[0]} is already in the list!");
+                    }
+                }
+                else if (tokens.Length == 4)
+                {
+                    if (namesOfGuests.Contains(tokens[0]))
+                    {
+                        namesOfGuests.Remove(tokens[0]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{tokens[0]} is not in the list!");
+                    }
+                }
+            }
+            foreach (var person in namesOfGuests)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
