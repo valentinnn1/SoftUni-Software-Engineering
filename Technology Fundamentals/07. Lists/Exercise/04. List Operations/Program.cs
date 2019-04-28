60f25e43778b1c6d30ad493b236d52e86a99c4b8
valentinnn1 <valentinnn1@outlook.com>
2019-04-28 21:06:22 +0300

    Added Part 2

---
+++ Technology Fundamentals/07. Lists/Exercise/04. List Operations/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._List_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] commandLine = command.Split();
+                ExecuteCommands(numbers, commandLine);
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void ExecuteCommands(List<int> numbers, string[] commandLine)
+        {
+            if (commandLine[0] == "Add")
+            {
+                numbers.Add(int.Parse(commandLine[1]));
+            }
+            else if (commandLine[0] == "Remove")
+            {
+                int index = int.Parse(commandLine[1]);
+                if (index >= 0 && index < numbers.Count)
+                {
+                    numbers.RemoveAt(index);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid index");
+                }
+            }
+            else if (commandLine[0] == "Insert")
+            {
+                int index = int.Parse(commandLine[2]);
+                int element = int.Parse(commandLine[1]);
+
+                if (index >= 0 && index < numbers.Count)
+                {
+                    numbers.Insert(index, element);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid index");
+                }
+            }
+            else if (commandLine[0] == "Shift")
+            {
+                string direction = commandLine[1];
+                int count = int.Parse(commandLine[2]);
+                ShiftList(numbers, direction, count);
+            }
+        }
+
+        private static void ShiftList(List<int> numbers, string direction, int count)
+        {
+            if (direction == "left")
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    int tempNum = numbers[0];
+                    numbers.Add(tempNum);
+                    numbers.RemoveAt(0);
+                }
+            }
+            else if (direction == "right")
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    numbers.Insert(0, numbers[numbers.Count - 1]);
+                    numbers.RemoveAt(numbers.Count - 1);
+                }
+            }
+        }
+    }
+}
