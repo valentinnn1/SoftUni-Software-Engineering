23ccd1157c1b85275a8db89d2fb2f0d26383c3dc
valentinnn1 <valentinnn1@outlook.com>
2019-04-28 21:05:21 +0300

    Added Part 2

---
+++ Technology Fundamentals/07. Lists/Exercise/07. Append Arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputData = Console.ReadLine()
+                .Split(new string[] { "|" }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            for (int i = 0; i < inputData.Count; i++)
+            {
+                string currentItem = inputData[i];
+                List<string> numbersStack = currentItem
+                    .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                inputData[i] = string.Join(" ", numbersStack);
+            }
+
+            for (int i = 0, j = inputData.Count - 1; i < j; i++, j--)
+            {
+                var temp = inputData[i];
+                inputData[i] = inputData[j];
+                inputData[j] = temp;
+
+            }
+            foreach (var item in inputData)
+            {
+                if (item != "")
+                {
+                    Console.Write(item + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Technology Fundamentals/07. Lists/Exercise/09. Pokemon Don't Go/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Pokemon_Don_t_Go
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> removedElements = new List<int>();
+
+            while (numbers.Any())
+            {
+                int indexToRemove = int.Parse(Console.ReadLine());
+
+                if (indexToRemove < 0)
+                {
+                    removedElements.Add(numbers[0]);
+                    numbers.Remove(numbers[0]);
+                    numbers.Insert(0, numbers[numbers.Count - 1]);
+
+                    IncreaseAndDecreaseValue(numbers, removedElements, indexToRemove);
+                    continue;
+                }
+                else if (indexToRemove > numbers.Count - 1)
+                {
+                    removedElements.Add(numbers[numbers.Count - 1]);
+                    numbers.RemoveAt(numbers.Count - 1);
+                    numbers.Insert(numbers.Count, numbers[0]);
+
+                    IncreaseAndDecreaseValue(numbers, removedElements, indexToRemove);
+                    continue;
+                }
+
+                removedElements.Add(numbers[indexToRemove]);
+                numbers.RemoveAt(indexToRemove);
+
+                IncreaseAndDecreaseValue(numbers, removedElements, indexToRemove);
+            }
+
+            Console.WriteLine(removedElements.Sum());
+        }
+
+        static void IncreaseAndDecreaseValue(List<int> numbers, List<int> removedElements, int indexToRemove)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] <= removedElements[removedElements.Count - 1])
+                {
+                    numbers[i] += removedElements[removedElements.Count - 1]; // increase the value of the current element with the removed one
+                }
+                else if (numbers[i] > removedElements[removedElements.Count - 1])
+                {
+                    numbers[i] -= removedElements[removedElements.Count - 1]; // decrease the value of the current element with the removed one
+                }
+            }
+        }
+    }
+}
