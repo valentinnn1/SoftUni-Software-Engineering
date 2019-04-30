53fb676f2f1d54e64b30ca668376e0a3b33ac785
valentinnn1 <valentinnn1@outlook.com>
2019-04-30 16:42:26 +0300

    Added Part 1

---
+++ Technology Fundamentals/08. Objects and Classes/Exercise/01. Advertisement Message/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Advertisement_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] phrases = { "Excellent product.",
+                "Such a great product.",
+                "I always use that product.",
+                "Best product of its category.",
+                "Exceptional product.",
+                "I can’t live without this product." };
+
+            string[] events = { "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!" };
+
+            string[] authors = { "Diana", "Petya", "Stella",
+                "Elena", "Katya", "Iva", "Annie", "Eva" };
+
+            string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+            Random number = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                int phrasesIndex = number.Next(phrases.Length);
+                int eventsIndex = number.Next(phrases.Length);
+                int authorsIndex = number.Next(phrases.Length);
+                int cityIndex = number.Next(phrases.Length);
+
+                Console.WriteLine($"{phrases[phrasesIndex]} " +
+                    $"{events[eventsIndex]} {authors[authorsIndex]} – {cities[cityIndex]}");
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals/08. Objects and Classes/Exercise/02. Articles/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Articles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ").ToArray();
+            int n = int.Parse(Console.ReadLine());
+            Article article = new Article();
+
+            article.Title = input[0];
+            article.Content = input[1];
+            article.Author = input[2];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(": ");
+
+                if (command[0] == "Edit")
+                {
+                    article.Edit(command[1]);
+                }
+                else if (command[0] == "ChangeAuthor")
+                {
+                    article.ChangeAuthor(command[1]);
+                }
+                else if (command[0] == "Rename")
+                {
+                    article.ChangeTitle(command[1]);
+                }
+            }
+            Console.WriteLine(article.ToString());
+        }
+    }
+
+    class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void Edit(string newContent)
+        {
+            Content = newContent;
+        }
+        public void ChangeAuthor(string newAuthor)
+        {
+            Author = newAuthor;
+        }
+        public void ChangeTitle(string newTitle)
+        {
+            Title = newTitle;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
