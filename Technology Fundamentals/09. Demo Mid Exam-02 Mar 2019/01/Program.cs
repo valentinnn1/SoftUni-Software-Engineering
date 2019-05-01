4a727e1817272a40fd790cb44de95ed4cc66806f
valentinnn1 <valentinnn1@outlook.com>
2019-05-01 10:07:58 +0300

    Updated

---
+++ Technology Fundamentals/09. Demo Mid Exam-02 Mar 2019/01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double flourPriceForPackage = double.Parse(Console.ReadLine());
+            double eggPriceForSingleEgg = double.Parse(Console.ReadLine());
+            double apronPriceForSingleApron = double.Parse(Console.ReadLine());
+            int freePackages = 0;
+            for (int i = 1; i <= students; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    freePackages++;
+                }
+            }
+
+            double price = apronPriceForSingleApron * (students + (Math.Ceiling(students * 0.2)))
+                + eggPriceForSingleEgg * 10 * students + flourPriceForPackage * (students - freePackages);
+
+            if (price <= budget)
+            {
+                Console.WriteLine($"Items purchased for {price:F2}$.");
+            }
+            else
+            {
+                Console.WriteLine($"{price - budget:F2}$ more needed.");
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals/09. Demo Mid Exam-02 Mar 2019/03/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int HighestTotalQuality = int.MinValue;
+            decimal HighestAvgQuality = decimal.MinValue;
+            int ShortestLenght = int.MaxValue;
+
+            string BestBread = "";
+
+            string input = Console.ReadLine();
+            if (input == "Bake It!")
+            {
+                return;
+            }
+
+            while (input != "Bake It!")
+            {
+                int[] currentBreadBanch = input
+                    .Split('#', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int SubTotalQuality = currentBreadBanch.Sum();
+                decimal SubAvgQuality = SubTotalQuality / currentBreadBanch.Length * decimal.One;
+                int SubShortestLenght = currentBreadBanch.Length;
+
+                if (SubTotalQuality > HighestTotalQuality)
+                {
+                    HighestTotalQuality = SubTotalQuality;
+                    HighestAvgQuality = SubAvgQuality;
+                    ShortestLenght = SubShortestLenght;
+                    BestBread = string.Join(" ", currentBreadBanch);
+                }
+                else if (SubTotalQuality == HighestTotalQuality
+                    && SubAvgQuality > HighestAvgQuality)
+                {
+                    HighestTotalQuality = SubTotalQuality;
+                    HighestAvgQuality = SubAvgQuality;
+                    ShortestLenght = SubShortestLenght;
+                    BestBread = string.Join(" ", currentBreadBanch);
+                }
+                else if (SubTotalQuality == HighestTotalQuality
+                    && SubAvgQuality == HighestAvgQuality
+                    && SubShortestLenght < ShortestLenght)
+                {
+                    HighestTotalQuality = SubTotalQuality;
+                    HighestAvgQuality = SubAvgQuality;
+                    ShortestLenght = SubShortestLenght;
+                    BestBread = string.Join(" ", currentBreadBanch);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Best Batch quality: {HighestTotalQuality}");
+            Console.WriteLine(BestBread);
+        }
+    }
+}
