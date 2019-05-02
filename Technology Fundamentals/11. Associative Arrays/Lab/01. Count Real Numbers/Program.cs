dc9ef82173e2ab019266d00432f75eb06816db3d
valentinnn1 <valentinnn1@outlook.com>
2019-05-02 17:07:24 +0300

    Added Part 1

---
+++ Technology Fundamentals/11. Associative Arrays/Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var counts = new SortedDictionary<double, int>();
+            foreach (int num in numbers)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts.Add(num, 1);
+                }
+            }
+            foreach (var num in counts)
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals/11. Associative Arrays/Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (!dict.ContainsKey(word))
+                {
+                    dict.Add(word, new List<string>());
+                }
+                dict[word].Add(synonym);
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
