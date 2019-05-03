67420c9fbadb12e89bf0eccba1b79ebfb9378b42
valentinnn1 <valentinnn1@outlook.com>
2019-05-03 15:53:05 +0300

    Added Part 2

---
+++ Technology Fundamentals/11. Associative Arrays/Lab/04. Largest 3 Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var top3 = Console.ReadLine().Split()
+                .Select(int.Parse).OrderByDescending(x => x).Take(3);
+
+            foreach (var item in top3)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals/11. Associative Arrays/Lab/05. Word Filter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05._Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine().Split().Where(x => x.Length % 2 == 0).ToList();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
