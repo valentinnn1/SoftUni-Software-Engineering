c3e547029888e2b93e180e98785a5aa3b2f3e968
valentinnn1 <valentinnn1@outlook.com>
2019-05-04 09:35:17 +0300

    Added Part 2

---
+++ Technology Fundamentals/11. Associative Arrays/Exercise/06. Courses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfCourses = new Dictionary<string, List<string>>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+                string[] info = input.Split(" : ");
+                string coursesName = info[0];
+                string studentName = info[1];
+
+                if (listOfCourses.ContainsKey(coursesName) == false)
+                {
+                    listOfCourses.Add(coursesName, new List<string>());
+                    listOfCourses[coursesName].Add(studentName);
+                }
+                else
+                {
+                    if (listOfCourses[coursesName].Contains(studentName) == false)
+                    {
+                        listOfCourses[coursesName].Add(studentName);
+                    }
+                }
+            }
+
+            var result = listOfCourses.OrderByDescending(x => x.Value.Count).ToList();
+            foreach (var kvp in result)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Count}");
+                var students = kvp.Value.OrderBy(x => x).ToList();
+                foreach (var student in students)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}
