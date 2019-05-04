18efc6b4e7f70e5ef687f42eec81a420ea52dfaa
valentinnn1 <valentinnn1@outlook.com>
2019-05-04 09:35:33 +0300

    Added Part 3

---
+++ Technology Fundamentals/11. Associative Arrays/Exercise/08. Company Users/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new SortedDictionary<string, List<string>>(); //key = nameOfCompany, value = list of id
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "End")
+                {
+                    break;
+                }
+                string[] info = input.Split(" -> ");
+                string company = info[0];
+                string employeeId = info[1];
+
+                if (dictionary.ContainsKey(company) == false)
+                {
+                    dictionary.Add(company, new List<string>());
+                    dictionary[company].Add(employeeId);
+                }
+                else
+                {
+                    if (dictionary[company].Contains(employeeId) == false)
+                    {
+                        dictionary[company].Add(employeeId);
+                    }
+                }
+            }
+
+            foreach (var kvp in dictionary)
+            {
+                Console.WriteLine(kvp.Key);
+                foreach (var employee in kvp.Value)
+                {
+                    Console.WriteLine($"-- {employee}");
+                }
+            }
+        }
+    }
+}
