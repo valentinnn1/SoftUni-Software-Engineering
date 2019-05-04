81682bda6ce398baeb98c42440a70817d001461d
valentinnn1 <valentinnn1@outlook.com>
2019-05-04 09:35:50 +0300

    Added Part 4

---
+++ Technology Fundamentals/11. Associative Arrays/Exercise/10. SoftUni Exam Results/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._SoftUni_Exam_Results
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentsAndPoints = new Dictionary<string, int>();
+            var submissions = new Dictionary<string, int>();
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "exam finished")
+            {
+                string[] inputLine = input.Split("-");
+                string name = inputLine[0];
+                if (inputLine[1] == "banned")
+                {
+                    studentsAndPoints.Remove(name);
+                }
+                else
+                {
+                    string language = inputLine[1];
+                    int points = int.Parse(inputLine[2]);
+
+                    if (!studentsAndPoints.ContainsKey(name))
+                    {
+                        studentsAndPoints[name] = points;
+                    }
+                    else if (points > studentsAndPoints[name])
+                    {
+                        studentsAndPoints[name] = points;
+                    }
+
+                    if (!submissions.ContainsKey(language))
+                    {
+                        submissions[language] = 0;
+                    }
+                    submissions[language]++;
+                }
+            }
+
+            Console.WriteLine("Results:");
+            foreach (var kvp in studentsAndPoints.OrderByDescending(x => x.Value)
+                .ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} | {kvp.Value}");
+            }
+
+            Console.WriteLine("Submissions:");
+            foreach (var kvp in submissions.OrderByDescending(x => x.Value)
+                .ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
+        }
+    }
+}
