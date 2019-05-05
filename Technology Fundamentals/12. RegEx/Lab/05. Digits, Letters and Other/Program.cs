4b82913dd49adda01ab4349b124c0da029dd71c3
valentinnn1 <valentinnn1@outlook.com>
2019-05-05 13:21:42 +0300

    Added Part 2

---
+++ Technology Fundamentals/12. RegEx/Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            var listOfDigits = new List<char>();
+            var listOfLetters = new List<char>();
+            var listOfOtherCharacters = new List<char>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    listOfDigits.Add(text[i]);
+                }
+
+                else if (char.IsLetter(text[i]))
+                {
+                    listOfLetters.Add(text[i]);
+                }
+
+                else
+                {
+                    listOfOtherCharacters.Add(text[i]);
+                }
+            }
+            Console.WriteLine(string.Join("", listOfDigits));
+            Console.WriteLine(string.Join("", listOfLetters));
+            Console.WriteLine(string.Join("", listOfOtherCharacters));
+        }
+    }
+}
---
+++ Technology Fundamentals/12. RegEx/Lab/06. Match Full Name/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _06._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"(\b[A-Z][a-z]+)([ ])([A-Z][a-z]+)\b";
+            Regex regex = new Regex(pattern);
+            MatchCollection matchNames = regex.Matches(text);
+            Console.WriteLine(string.Join(" ", matchNames));
+        }
+    }
+}
