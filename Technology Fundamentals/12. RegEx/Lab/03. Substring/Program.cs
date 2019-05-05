c0826739569eec741ecfcea0ab0339f98178afa2
valentinnn1 <valentinnn1@outlook.com>
2019-05-05 13:21:13 +0300

    Added Part 1

---
+++ Technology Fundamentals/12. RegEx/Lab/03. Substring/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToLower();
+            string text = Console.ReadLine().ToLower();
+
+            while (text.Contains(word))
+            {
+                int index = text.IndexOf(word);
+                text = text.Remove(index, word.Length);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
