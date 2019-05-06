e94c189d70fbd764a37cae6cabd3bff9cd77e2ce
valentinnn1 <valentinnn1@outlook.com>
2019-05-06 09:18:26 +0300

    Added Part 3

---
+++ Technology Fundamentals/12. RegEx/Lab/07. Match Dates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _07._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(\d{2})([-.\/])([A-Z][a-z]{2})(\2)(\d{4})";
+            Regex regex = new Regex(pattern);
+            MatchCollection matchDates = regex.Matches(input);
+
+            foreach (Match match in matchDates)
+            {
+                Console.WriteLine($"Day: {match.Groups[1].Value}, Month: {match.Groups[3].Value}, Year: {match.Groups[5].Value}");
+            }
+        }
+    }
+}
