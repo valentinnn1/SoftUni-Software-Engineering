26ab45006e81e01d8ae207a6254af2e90f347c33
valentinnn1 <valentinnn1@outlook.com>
2019-05-07 23:02:39 +0300

    Added Part 1

---
+++ Technology Fundamentals/12. RegEx/More Exercise/02. Ascii Sumator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Ascii_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstCharacter = char.Parse(Console.ReadLine());
+            char secondCharacter = char.Parse(Console.ReadLine());
+            string randomString = Console.ReadLine();
+
+            List<char> charactersBetwenTwo = CharactersBetwenTwo(firstCharacter, secondCharacter, randomString);
+
+            int sumOfThisCharacters = SumOfAsciiCode(charactersBetwenTwo);
+
+            Console.WriteLine(sumOfThisCharacters);
+        }
+
+        private static int SumOfAsciiCode(List<char> charactersBetwenTwo)
+        {
+            int sum = 0;
+
+            foreach (char item in charactersBetwenTwo)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        private static List<char> CharactersBetwenTwo(char firstCharacter, char secondCharacter, string randomString)
+        {
+            List<char> charactersBetwenTwo = new List<char>();
+
+            int maxChar = Math.Max(firstCharacter, secondCharacter);
+            int minChar = Math.Min(firstCharacter, secondCharacter);
+
+            for (int i = 0; i < randomString.Length; i++)
+            {
+                if (randomString[i] > minChar && randomString[i] < maxChar)
+                {
+                    charactersBetwenTwo.Add(randomString[i]);
+                }
+            }
+            return charactersBetwenTwo;
+        }
+    }
+}
