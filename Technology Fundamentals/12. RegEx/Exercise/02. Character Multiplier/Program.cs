24df8a83fb99927917c0a0222fd019e1df9bbb81
valentinnn1 <valentinnn1@outlook.com>
2019-05-07 23:01:08 +0300

    Added Part 1

---
+++ Technology Fundamentals/12. RegEx/Exercise/02. Character Multiplier/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Character_Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split();
+
+            string firstWord = tokens[0];
+            string secondWord = tokens[1];
+
+            string longerWord = string.Empty;
+            string shorterWord = string.Empty;
+
+            int totalSum = 0;
+
+            if (firstWord.Length >= secondWord.Length)
+            {
+                longerWord = firstWord;
+                shorterWord = secondWord;
+            }
+            else
+            {
+                longerWord = secondWord;
+                shorterWord = firstWord;
+            }
+
+            for (int i = 0; i < shorterWord.Length; i++)
+            {
+                totalSum += longerWord[i] * shorterWord[i];
+            }
+            for (int j = shorterWord.Length; j < longerWord.Length; j++)
+            {
+                totalSum += longerWord[j];
+            }
+            Console.WriteLine(totalSum);
+        }
+    }
+}
---
+++ Technology Fundamentals/12. RegEx/Exercise/03. Extract File/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            int startIndexOfFile = path.LastIndexOf('\\') + 1;
+            string file = path.Substring(startIndexOfFile);
+            int startIndexOfExtension = file.LastIndexOf('.') + 1;
+            string nameOfFile = file.Substring(0, startIndexOfExtension - 1);
+            string extensionOfFile = file.Substring(startIndexOfExtension);
+
+            Console.WriteLine($"File name: {nameOfFile}");
+            Console.WriteLine($"File extension: {extensionOfFile}");
+        }
+    }
+}
