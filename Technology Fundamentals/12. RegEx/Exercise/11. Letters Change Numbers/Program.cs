a20f15891b210d1e00ec8e122f5b2103102883fc
valentinnn1 <valentinnn1@outlook.com>
2019-05-07 23:01:46 +0300

    Added Part 4

---
+++ Technology Fundamentals/12. RegEx/Exercise/11. Letters Change Numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._Letters_Change_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+
+                .Split(new[] { " ", "\t" }, StringSplitOptions.RemoveEmptyEntries);
+            decimal sum = 0M;
+
+            foreach (var word in words)
+            {
+                char beforeSymbol = word[0];
+                char afterSymbol = word[word.Length - 1];
+                decimal number = decimal.Parse(string.Concat(word.Skip(1).Take(word.Length - 2)));
+
+                if (char.IsUpper(beforeSymbol))
+                {
+                    number /= beforeSymbol - 64;
+                }
+                else
+                {
+                    number *= beforeSymbol - 96;
+                }
+
+                if (char.IsUpper(afterSymbol))
+                {
+                    number -= afterSymbol - 64;
+                }
+                else
+                {
+                    number += afterSymbol - 96;
+                }
+                sum += number;
+            }
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
