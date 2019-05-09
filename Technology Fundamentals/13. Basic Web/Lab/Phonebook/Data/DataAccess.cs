48bfee3f0c50a30f64b721bc4934324afd109c1c
valentinnn1 <valentinnn1@outlook.com>
2019-05-09 18:47:49 +0300

    Added 13

---
+++ Technology Fundamentals/13. Basic Web/Lab/Phonebook/Data/DataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Phonebook.Data.Models;
+
+namespace Phonebook.Data
+{
+    public class DataAccess
+    {
+        static DataAccess()
+        {
+            Contacts = new List<Contact>();
+        }
+        public static List<Contact> Contacts { get; set; }
+    }
+}
