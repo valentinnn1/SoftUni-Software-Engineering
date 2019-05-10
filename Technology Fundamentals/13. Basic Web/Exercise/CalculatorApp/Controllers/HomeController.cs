9f328f72a0f0110981ba26decb66df3fd9087304
valentinnn1 <valentinnn1@outlook.com>
2019-05-10 09:47:09 +0300

    Added 13

---
+++ Technology Fundamentals/13. Basic Web/Exercise/CalculatorApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(Calculator calulator)
+        {
+            return View(calulator);
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(Calculator calculator)
+        {
+            calculator.CalculateResult();
+            return RedirectToAction("Index", calculator);
+        }
+    }
+}
