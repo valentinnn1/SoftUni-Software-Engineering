2bc70d45b4f71f199bf3517f96b554330456826f
valentinnn1 <valentinnn1@outlook.com>
2019-05-12 21:15:00 +0300

    Added 15

---
+++ Technology Fundamentals/15. Demo Final Exam-06 Apr 2019/03/Library/Controllers/LibraryController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.Data;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var db = new LibraryDbContext())
+            {
+                var allBooks = db.Books.ToList();
+                return this.View(allBooks);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string title, string author, double price)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                return RedirectToAction("Index");
+            }
+            using (var db = new LibraryDbContext())
+            {
+                Book books = new Book
+                {
+                    Title = title,
+                    Author = author,
+                    Price = price,
+                };
+                db.Books.Add(books);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db = new LibraryDbContext())
+            {
+                var bookToEdit = db.Books.FirstOrDefault(x => x.Id == id);
+
+                if (bookToEdit == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return this.View(bookToEdit);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book books)
+        {
+            using (var db = new LibraryDbContext())
+            {
+                var bookToEdit = db.Books.FirstOrDefault(x => x.Id == books.Id);
+                bookToEdit.Title = books.Title;
+                bookToEdit.Author = books.Author;
+                bookToEdit.Price = books.Price;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (var db = new LibraryDbContext())
+            {
+                var bookToDelete = db.Books.FirstOrDefault(x => x.Id == id);
+                if (bookToDelete == null)
+                {
+                    RedirectToAction("Index");
+                }
+                return this.View(bookToDelete);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Book books)
+        {
+            using (var db = new LibraryDbContext())
+            {
+                db.Books.Remove(books);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Technology Fundamentals/15. Demo Final Exam-06 Apr 2019/03/Library/Data/LibraryDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        private const string ConnectionString = @"Server=.\SQLEXPRESS;DataBase=LibraryDb;Integrated Security=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
