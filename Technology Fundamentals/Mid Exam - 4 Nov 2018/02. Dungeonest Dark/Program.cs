4e381aff5a0c3e7f947f436cd00f549d74dea727
valentinnn1 <valentinnn1@outlook.com>
2019-05-15 15:54:07 +0300

    Updated

---
+++ Technology Fundamentals/Mid Exam - 4 Nov 2018/02. Dungeonest Dark/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Dungeonest_Dark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rooms = Console.ReadLine().Split('|').ToArray();
+            int health = 100;
+            int coins = 0;
+
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                string[] currentRoom = rooms[i].Split().ToArray();
+                string command = currentRoom[0];
+                int number = int.Parse(currentRoom[1]);
+
+                if (command == "potion")
+                {
+                    int currentHealth = health;
+                    health += number;
+                    if (health > 100)
+                    {
+                        Console.WriteLine($"You healed for {100 - currentHealth} hp.");
+                        Console.WriteLine($"Current health: 100 hp.");
+                        health = 100;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You healed for {number} hp.");
+                        Console.WriteLine($"Current health: {health} hp.");
+                    }
+                }
+                else if (command == "chest")
+                {
+                    coins += number;
+                    Console.WriteLine($"You found {number} coins.");
+                }
+                else
+                {
+                    health -= number;
+                    if (health > 0)
+                    {
+                        Console.WriteLine($"You slayed {command}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You died! Killed by {command}.");
+                        Console.WriteLine($"Best room: {i+1}");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"You've made it!");
+            Console.WriteLine($"Coins: {coins}");
+            Console.WriteLine($"Health: {health}");
+        }
+    }
+}
---
+++ Technology Fundamentals/Mid Exam - 4 Nov 2018/03. Quests Journal/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Quests_Journal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listOfPhrases = Console.ReadLine()
+                .Split(", ")
+                .ToList();
+            string command;
+
+            while ((command = Console.ReadLine()) != "Retire!")
+            {
+                string[] commandLine = command.Split(" - ").ToArray();
+                string currentCommand = commandLine[0];
+                string quest = commandLine[1];
+
+                switch (currentCommand)
+                {
+                    case "Start":
+                        if (!listOfPhrases.Contains(quest))
+                        {
+                            listOfPhrases.Add(quest);
+                        } break;
+                    case "Complete":
+                        if (listOfPhrases.Contains(quest))
+                        {
+                            listOfPhrases.Remove(quest);
+                        } break;
+                    case "Side Quest":
+                        List<string> listOfSideQuest = quest.Split(':').ToList();
+                        string leftSide = listOfSideQuest[0];
+                        string rightSide = listOfSideQuest[1];
+
+                        if (listOfPhrases.Contains(leftSide))
+                        {
+                            int index = listOfPhrases.IndexOf(leftSide);
+                            if (!listOfPhrases.Contains(rightSide))
+                            {
+                                listOfPhrases.Insert(index + 1, rightSide);
+                            }
+                        }
+                        break;
+                    case "Renew":
+                        if (listOfPhrases.Contains(quest))
+                        {
+                            listOfPhrases.Remove(quest);
+                            listOfPhrases.Add(quest);
+                        } break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(", ", listOfPhrases));
+        }
+    }
+}
