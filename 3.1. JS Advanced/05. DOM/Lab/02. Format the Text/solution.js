89e63bc5d6df14da64c14236f2f4fcb28a9b1d3b
valentinnn1 <valentinnn1@outlook.com>
2019-10-05 16:32:49 +0300

    Updated

---
+++ 3.1. JS Advanced/05. DOM/Lab/02. Format the Text/solution.js
@@ -0,0 +1,21 @@
+function solve() {
+  let input = document.getElementById("input");
+  let output = document.getElementById("output");
+
+  if (input === null || output === null) {
+    throw new Error("Something is wrong...");
+  }
+
+  let sentences = input.innerHTML.split(".").filter(x => x !== "");
+  for (let i = 0; i < sentences.length; i += 3) {
+    let p = document.createElement("p");
+    let str = "";
+    for (let j = 0; j < 3; j++) {
+      if (i + j < sentences.length) {
+        str += sentences[i + j] + ".";
+      }
+    }
+    p.innerHTML = str;
+    output.appendChild(p);
+  }
+}
