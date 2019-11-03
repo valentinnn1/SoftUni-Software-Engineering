f3306cdacaab3dd74a6b7cfdfcc81328b0ebb2d1
valentinnn1 <valentinnn1@outlook.com>
2019-11-03 12:00:33 +0200

    Updated

---
+++ 3.2. JS Applications/01. This/Exercise/03. HEX.js
@@ -0,0 +1,37 @@
+class Hex {
+  constructor(value) {
+    this.value = value;
+  }
+
+  valueOf() {
+    return this.value;
+  }
+
+  toString() {
+    return "0x" + this.value.toString(16).toLocaleUpperCase();
+  }
+
+  plus(number) {
+    if (number instanceof Hex) {
+      return new Hex(this.value + number);
+    }
+  }
+
+  minus(number) {
+    if (number instanceof Hex) {
+      return new Hex(this.value - number);
+    }
+  }
+
+  parse(string) {
+    this.value = parseInt(string, 16);
+    return this;
+  }
+}
+let FF = new Hex(255);
+console.log(FF.toString());
+FF.valueOf() + 1 == 256;
+let a = new Hex(10);
+let b = new Hex(5);
+console.log(a.plus(b).toString());
+console.log(a.plus(b).toString() === "0xF");
